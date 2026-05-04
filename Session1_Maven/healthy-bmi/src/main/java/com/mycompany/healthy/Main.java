c44e4bf788fc2a6c4b454fa6828ceb41faed4677
Nguyen Trung Hieu (FPT University) <trunghieu291005@gmail.com>
2026-05-04 10:57:48 +0700

    add session 1 maven

---
+++ Session1_Maven/healthy-bmi/src/main/java/com/mycompany/healthy/Main.java
@@ -0,0 +1,12 @@
+package com.mycompany.healthy;
+
+import com.mycompany.heath.core.BmiCalculator;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println( BmiCalculator.getBmi(63,1.7));
+
+    }
+}
---
+++ Session1_Maven/maven-to-go/src/main/java/com/mycompany/basis/Main.java
@@ -0,0 +1,11 @@
+package com.mycompany.basis;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven and JDK-21");
+        System.out.println("Toi la 1 exercatabl char - toi co the chay ngay tu dong lenh - v83.86 " );
+        System.out.println("Chuc mung ban da chay thanh cong");
+    }
+}
