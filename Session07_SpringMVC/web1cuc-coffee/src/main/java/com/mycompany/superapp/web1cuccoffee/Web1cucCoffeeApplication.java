616b2bb5ad5b92c6c743fb41d0ed86bf788c6f1d
trunghieu2910 <trunghieu291005@gmail.com>
2026-05-10 20:32:06 +0700

    add new projrect

---
+++ Session07_SpringMVC/web1cuc-coffee/src/main/java/com/mycompany/superapp/web1cuccoffee/Web1cucCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.superapp.web1cuccoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web1cucCoffeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web1cucCoffeeApplication.class, args);
+    }
+
+}
---
+++ Session07_SpringMVC/web1cuc-coffee/src/test/java/com/mycompany/superapp/web1cuccoffee/Web1cucCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.superapp.web1cuccoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web1cucCoffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
