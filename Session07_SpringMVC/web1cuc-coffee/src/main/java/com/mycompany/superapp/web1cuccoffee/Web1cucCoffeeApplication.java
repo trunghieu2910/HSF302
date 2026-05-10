ee6b9a2fa10a9bf63fc00d589b914143cbb3a9da
trunghieu2910 <trunghieu291005@gmail.com>
2026-05-10 20:43:19 +0700

    udate to start

--- Session07_SpringMVC/web1cuc-coffee/src/main/java/com/mycompany/superapp/web1cuccoffee/Web1cucCoffeeApplication.java
+++ Session07_SpringMVC/web1cuc-coffee/src/main/java/com/mycompany/superapp/web1cuccoffee/Web1cucCoffeeApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Web1cucCoffeeApplication {
-
+    // Bean trung tâm : nơi chứa về những khai báo về tất cả các loại Bean trong app
+    // Dependency Object là những đứa có @Component, @Service, @Repository, @Controller, @RestController
+    // @SpringBootApplication gộp sẵn 3 @ khác  :
+    // Configuration , @Component, @EnableAutoConfiguration
     public static void main(String[] args) {
         SpringApplication.run(Web1cucCoffeeApplication.class, args);
     }
